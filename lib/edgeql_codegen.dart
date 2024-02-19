edgedb/edgedb-dart

commit 08d47cbcd05c4213582ab0b72473f089c52a1956

Add basic support for protocol v2 (#29)

lib/edgeql_codegen.dart
@@ -352,6 +352,10 @@ _WalkCodecReturn _walkCodec(Codec codec, LibraryBuilder file,
                 'package:edgedb/src/codecs/codecs.dart')
             .newInstance([
           literalString(codec.tid),
+          if (codec is NamedTupleCodec)
+            codec.typeName != null
+                ? literalString(codec.typeName!)
+                : literalNull,
           literalList(codecs),
           literalList(names),
           if (codec is ObjectCodec) literalList(cards),
@@ -386,6 +390,14 @@ _WalkCodecReturn _walkCodec(Codec codec, LibraryBuilder file,
                 'package:edgedb/src/codecs/codecs.dart')
             .newInstance([
           literalString(codec.tid),
+          if (codec is ArrayCodec)
+            codec.typeName != null
+                ? literalString(codec.typeName!)
+                : literalNull,
+          if (codec is RangeCodec)
+            codec.typeName != null
+                ? literalString(codec.typeName!)
+                : literalNull,
           child.codecExpr,
           if (codec is ArrayCodec) literalNum(codec.length)
         ], {}, [
@@ -443,6 +455,7 @@ _WalkCodecReturn _walkCodec(Codec codec, LibraryBuilder file,
         Reference('TupleCodec', 'package:edgedb/src/codecs/codecs.dart')
             .newInstance([
           literalString(codec.tid),
+          codec.typeName != null ? literalString(codec.typeName!) : literalNull,
           literalList(codecs),
         ], {
           if (!isArgsCodec)

lib/src/base_proto.dart
@@ -17,7 +17,7 @@ import 'primitives/transport.dart';
 import 'primitives/types.dart';
 import 'utils/indent.dart';
 
-const protoVer = ProtocolVersion(1, 0);
+const protoVer = ProtocolVersion(2, 0);
 const minProtoVer = ProtocolVersion(1, 0);
 
 enum Capabilities {

lib/src/codecs/array.dart
@@ -26,8 +26,9 @@ import 'codecs.dart';
 class ArrayCodec<T> extends Codec {
   final Codec subCodec;
   final int length;
+  final String? typeName;
 
-  ArrayCodec(super.tid, this.subCodec, this.length);
+  ArrayCodec(super.tid, this.typeName, this.subCodec, this.length);
 
   @override
   void encode(WriteBuffer buf, dynamic object) {

lib/src/codecs/boolean.dart
@@ -21,7 +21,7 @@ import '../primitives/buffer.dart';
 import 'codecs.dart';
 
 class BoolCodec extends ScalarCodec {
-  BoolCodec(super.tid);
+  BoolCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields

lib/src/codecs/bytes.dart
@@ -23,7 +23,7 @@ import '../primitives/buffer.dart';
 import 'codecs.dart';
 
 class BytesCodec extends ScalarCodec {
-  BytesCodec(super.tid);
+  BytesCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields

lib/src/codecs/codecs.dart
@@ -56,14 +56,16 @@ abstract class Codec {
 }
 
 abstract class ScalarCodec extends Codec {
-  ScalarCodec(super.tid);
+  final String? typeName;
+
+  ScalarCodec(super.tid, this.typeName);
 
   final String returnType = 'dynamic';
   final String? returnTypeImport = null;
   final String? argType = null;
 
-  derive(String tid) {
-    return _scalarCodecConstructors[this.tid]!(this.tid);
+  derive(String tid, String? typeName) {
+    return _scalarCodecConstructors[this.tid]!(tid, typeName);
   }
 
   @override
@@ -115,7 +117,7 @@ final _scalarCodecConstructors = {
   'cal::date_duration': DateDurationCodec.new,
   'cfg::memory': ConfigMemoryCodec.new,
   'ext::pgvector::vector': PgVectorCodec.new,
-}.map<String, ScalarCodec Function(String)>((typename, type) {
+}.map<String, ScalarCodec Function(String, String?)>((typename, type) {
   final id = knownTypeNames[typename];
   if (id == null) {
     throw InternalClientError("unknown codec type name");
@@ -125,5 +127,5 @@ final _scalarCodecConstructors = {
 
 final scalarCodecs = {
   for (var codec in _scalarCodecConstructors.entries)
-    codec.key: codec.value(codec.key)
+    codec.key: codec.value(codec.key, knownTypes[codec.key])
 };

lib/src/codecs/datetime.dart
@@ -25,7 +25,7 @@ const timeshift = 946684800000000;
 final epochDate = DateTime.utc(2000, 1, 1);
 
 class DateTimeCodec extends ScalarCodec {
-  DateTimeCodec(super.tid);
+  DateTimeCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields
@@ -49,7 +49,7 @@ class DateTimeCodec extends ScalarCodec {
 }
 
 class LocalDateTimeCodec extends ScalarCodec {
-  LocalDateTimeCodec(super.tid);
+  LocalDateTimeCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields
@@ -78,7 +78,7 @@ class LocalDateTimeCodec extends ScalarCodec {
 }
 
 class LocalDateCodec extends ScalarCodec {
-  LocalDateCodec(super.tid);
+  LocalDateCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields
@@ -107,7 +107,7 @@ class LocalDateCodec extends ScalarCodec {
 }
 
 class LocalTimeCodec extends ScalarCodec {
-  LocalTimeCodec(super.tid);
+  LocalTimeCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields
@@ -134,7 +134,7 @@ class LocalTimeCodec extends ScalarCodec {
 }
 
 class DurationCodec extends ScalarCodec {
-  DurationCodec(super.tid);
+  DurationCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields
@@ -165,7 +165,7 @@ class DurationCodec extends ScalarCodec {
 }
 
 class RelativeDurationCodec extends ScalarCodec {
-  RelativeDurationCodec(super.tid);
+  RelativeDurationCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields
@@ -198,7 +198,7 @@ class RelativeDurationCodec extends ScalarCodec {
 }
 
 class DateDurationCodec extends ScalarCodec {
-  DateDurationCodec(super.tid);
+  DateDurationCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields

lib/src/codecs/enum.dart
@@ -19,7 +19,7 @@
 import "text.dart";
 
 class EnumCodec extends StrCodec {
-  EnumCodec(super.tid);
+  EnumCodec(super.tid, super.typeName);
 
   @override
   // ignore: overridden_fields

commit f1e7a14219f8370ff2ff323bddc8206dcc7eb621

Fix regression in enum type codegen introduced in 08d47cb + add test

lib/edgeql_codegen.dart
@@ -265,7 +265,7 @@ _WalkCodecReturn _walkCodec(Codec codec, LibraryBuilder file,
               : null),
         codec is EnumCodec
             ? Reference('EnumCodec', 'package:edgedb/src/codecs/codecs.dart')
-                .newInstance([literalString(codec.tid)])
+                .newInstance([literalString(codec.tid), literalNull])
             : Reference('scalarCodecs', 'package:edgedb/src/codecs/codecs.dart')
                 .index(literalString(codec.tid))
                 .nullChecked);

test/codegen_tests/lib/complexParams.edgeql
@@ -5,6 +5,7 @@ select {
   namedTup := <tuple<a: str, b: int64>>$namedTup,
   arrayTup := <array<tuple<str, bool>>>$arrayTup,
   optTup := <optional tuple<str, int64>>$optTup,
+  enumTest := sys::VersionStage.dev,
 
   # test (empty) set decoding
   multi mStr := 'test',